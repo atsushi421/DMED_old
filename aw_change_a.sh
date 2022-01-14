60d12356ce578cea33279bb842de920645b4495f
atsushi421 <a.yano.578@ms.saitama-u.ac.jp>
2022-01-14 13:13:29 +0900

    add eva

--- aw_change_a.sh
+++ aw_change_a.sh
@@ -1,22 +1,7 @@
 #!/usr/bin/bash
 
 # remove result
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_1/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_2/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_3/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_4/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_5/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_6/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_7/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_8/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_9/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_10/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_11/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_12/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_13/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_14/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_15/*.txt
-rm /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/coreNum_16/*.txt
+find /mnt/c/Users/atsushi/Documents/Study/M1/master_thesis/Code/result/Autoware/aw_change_a/ -name *.txt | xargs rm
 wait
 
 
